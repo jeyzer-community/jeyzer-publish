cc947876b48b2cf76e9d48ab4d5a5dd2920a4a98
Jeyzer Admin <admin@jeyzer.org>
2021-01-06 22:01:36 +0100

    Java module support

---
+++ src/main/java9/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * Jeyzer publish module
+ */
+module org.jeyzer.publish {
+	
+	exports org.jeyzer.mx.event;
+	exports org.jeyzer.publish;
+	exports org.jeyzer.mx;
+	exports org.jeyzer.publish.event;
+
+	requires java.management;
+	
+}
